d747c4da4fd24fd6e2368de7ee3ad102bbd83928
Isabela Comegna <isacomegna@gmail.com>
2019-11-17 17:12:51 -0300

    Ignora token de autenticidade

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ComplaintsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @complaints = Complaint.all
   end
